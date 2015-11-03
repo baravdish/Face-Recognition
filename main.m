18fa5a7affe181a1a3b59388a994a1f7295ccec0
Gabriel Baravdish <gabrielbaravdish@gmail.com>
2015-11-03 17:36:02 +0100

    Added load mat-file DB. Added Otsu's method for normalization. Added a test between Otsu's method and built-int function

--- main.m
+++ main.m
@@ -1,8 +1,9 @@
+%% Normalization part of main.m
 clear
 clc
 
-% TODO: LÄS IN DB. PRE-COMPUTED
-% load mat
+tic
+load DB
 
 nImages = 4;
 path = 'DB0\db0_';
@@ -10,15 +11,27 @@ fileformat = '.jpg';
 
 for i = 1:nImages
     
-  img = imread(strcat(path, num2str(i), fileformat));
-  figure;
-  imshow(img);
-  
-  % TODO: Pipeline för face recognition
-  
-  
-  
+%     img = imread(strcat(path, num2str(i), fileformat));
+%     img = rgb2gray(img);
+    img = album{i};
+    normImg = otsuNormalize(img);
+    figure
+    imshow(normImg)
+    
 end
+% save DB.mat
+toc
 
-pause
-close all
+%% TEST part
+% - A test to check the difference between this implementation and the built-in function graythresh()
+
+for n = 1:4
+    test = threshImages{n};
+    sumMy = sum(test(:));
+    a = album{n};
+    level = graythresh(a);
+    sumReal = a > 255*level;
+    sumReal = abs(sum(sumReal(:)));
+    difference = abs(sumMy - sumReal);
+    difference/(256*256)
+end
---
+++ otsuNormalize.m
@@ -0,0 +1,29 @@
+function [normalizedImg] = otsuNormalize(img)
+
+    [bins, binIndexVec] = imhist(img);
+
+    nPixels = length(img(:));
+    probabilityBins = bins/nPixels;
+    nBins = length(binIndexVec);
+    
+    WbVec = zeros(nBins, 1);
+    WfVec = zeros(nBins, 1);
+    sumBVec = zeros(nBins, 1);
+
+    for k = 1:nBins
+        WbVec(k) = sum(bins(1:k));
+        sumBVec(k) = binIndexVec(1:k)'*bins(1:k);
+    end
+
+    WfVec = nPixels - WbVec;
+
+    sumTerm = sum(binIndexVec'*bins);
+    meanBack = sumBVec./WbVec;
+    meanForground = (sumTerm - sumBVec)./WfVec;
+    inBetween = WbVec.*WfVec.*(meanBack - meanForground).^2;
+
+    [~, threshold] = max(inBetween);
+
+    normalizedImg = img > threshold;
+
+end
