afb71624bd6a38475b27abf189e9f267c342d16a
lasse93 <larbe444@student.liu.se>
2015-11-19 09:39:15 +0100

    kommentarer

--- eigenface.m
+++ eigenface.m
@@ -7,6 +7,7 @@ addpath src
 [access_images, number_of_access_images] = readAllFromDir('access', 'img/access/', '*.jpg');
 number_of_access_images = 10;
 
+%A = [200 000, antal inlästa bilder]
 A = zeros(500*400, number_of_access_images);
 average_face = zeros(500*400, 1);
 image_vector = zeros(500*400, number_of_access_images);
@@ -16,6 +17,7 @@ for k = 1 : number_of_access_images
   image_vector(:, k) = double(reshape(resized_image, [500*400 1])) / 255;
   average_face  = average_face + image_vector(:, k);
 end
+%normalisera average_face
 average_face = average_face / number_of_access_images;
 
 colormap gray
@@ -28,6 +30,7 @@ end
 
 L = A'*A;
 
+%V är en matris med egenvektorer och D är en diagonal matris med egenvärdena
 [V,D] = eig(L);
 
 eigenfaces = zeros(500*400, number_of_access_images);
@@ -36,6 +39,7 @@ figure
 colormap gray
 for l = 1 : number_of_access_images
   for k = 1 : number_of_access_images
+    %ekv. (6) artikel Eigenfaces_Turk sida 75
     eigenfaces(:, l) = eigenfaces(:, l) + V(l,k) * A(:,k);
   end
   subplot(4,4,l)
@@ -45,13 +49,13 @@ end
 known_omegas = zeros(number_of_access_images, number_of_access_images);
 for l = 1 : number_of_access_images
   for k = 1 : number_of_access_images
+    %ekv. (7) artikel Eigenfaces_Turk sida 75
     known_omegas(k, l) = eigenfaces(:, k)' * (image_vector(:, l) - mean(image_vector(:, l)));
   end
 end
 
 % Verify
-
-image_to_check = imread('img/test.jpg');;
+image_to_check = imread('img/test.jpg');
 image_to_check_gray = rgb2gray(image_to_check);
 image_to_check_resized = imresize(image_to_check_gray, [500 400]);
 image_to_check_vector = double(reshape(image_to_check_resized, [500*400 1])) / 255;
@@ -63,7 +67,9 @@ end
 
 
 for k = 1 : number_of_access_images
+  %ekv. (8) artikel Eigenfaces_Turk sida 76
   distances(k) = norm(unknown_omega - known_omegas(:, k));
 end
 
+%printar ut distansen, lågt värde = ansiktet tillhör databasen
 distances / max(distances)
